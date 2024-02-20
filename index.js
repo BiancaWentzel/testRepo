0abbca6174b4e916091813b39897d657cca1decb
Bianca Wentzel <bianca.wentzel@fokus.fraunhofer.de>
2024-02-20 10:09:10 +0100

    added function

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ function makeAlert(str){
     console.log(str)
 }
 
+function screamForHelp() {
+    console.log("HELP!");
+}
+
 
 function scndFunc(str){}
 
-module.exports = makeAlert, scndFunc
+module.exports = makeAlert, scndFunc, screamForHelp
